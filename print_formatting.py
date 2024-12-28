dc1bef934f551b0d9f8c054fdff331bcdc9dfabd
shreyassp25 <shreyassp03@gmail.com>
2024-12-28 14:17:41 +0530

    add print formatting

--- print_formatting.py
+++ print_formatting.py
@@ -5,3 +5,23 @@
 name=input("enter your name: ")
 print("hello there {}".format(name))
 
+
+print("the {2} {1} {0}".format("fox","brown","quick"))
+    
+print("my name is {a} {b}".format(a="anthony",b="golsalves"))
+
+### float formatting follows the synatx-   {value:width.precisionf}
+
+
+result=100/77
+print(result)
+print("result is {r:1.3f}.".format(r=result))
+
+#####  string literal method
+
+print(f"hello there {name}")
+
+
+
+
+
