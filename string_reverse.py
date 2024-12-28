27c7723ea12cd687462e920adb684b5023ba88d2
shreyassp25 <shreyassp03@gmail.com>
2024-12-28 13:44:22 +0530

    add code

---
+++ string_reverse.py
@@ -0,0 +1,4 @@
+my_str="This is my string"
+rev=my_str[::-1]
+
+print(rev)
---
+++ string_slicing(indexing).py
@@ -0,0 +1,8 @@
+my_string="this is a string"
+
+new_string=my_string[0:4]
+
+print(new_string)
+
+slicestr=my_string[0:15:2]
+print(slicestr)
