63c33589b21fd6ec7690256d2a241281705fe6bf
shreyassp25 <shreyassp03@gmail.com>
2024-12-30 10:25:27 +0530

    new code

--- lists.py
+++ lists.py
@@ -1,3 +1,10 @@
 ## lists are a kind of data structure in python....used to hold different kinds of objects
+## lists are mutable
+
+list=[1,3,2,9,8,5,6]
+list.append(10)
+print(list)
+new=list.pop()
+print (new)
 
 
