a272fb8e4fd64e6c3e24ec9967a707fc5138a119
shreyassp25 <shreyassp03@gmail.com>
2024-12-30 11:09:58 +0530

    add code

---
+++ tuples.py
@@ -0,0 +1,15 @@
+##tuples are similar to lists but are immutable
+## tuples use parenthesis instead of square brackets
+
+tuple=(1,2,3,4,5)
+#for last element of tuple
+print(tuple[-1])
+
+t=("a","b","b","c")
+print(t.count("a"))
+
+
+
+
+
+
