29dc534b36e56f7e072ea104bbd68e4de738ffee
shreyassp25 <shreyassp03@gmail.com>
2025-10-06 19:24:55 +0530

    add new code

---
+++ loops/question1.py
@@ -0,0 +1,11 @@
+# Write a program to print multiplication table of a given number using for loop.
+
+a=1
+while(a<11):
+    print(3*a)
+    a=a+1
+
+for i in range(1,11):
+    print(3*i)
+
+    
---
+++ loops/question3.py
@@ -0,0 +1,4 @@
+# Write a program to find whether a given number is prime or not.
+
+a=7
+for i in range
