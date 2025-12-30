71dd05683cee6caa501c7ba522e84a996069b317
shreyassp25 <shreyassp03@gmail.com>
2025-12-30 14:21:42 +0530

    add new code

---
+++ functions & recursion/functions.py
@@ -0,0 +1,19 @@
+# functions are to reuse a block of code
+
+# code to use functions to get the average of 2 numbers
+
+# when u use return in average... 
+
+def average(a,b):
+    average = (a+b)/2
+    return average
+
+a=int(input("ebter number 1 : "))
+b=int(input("ebter number 2 : "))
+
+avg=average(a,b)
+
+print("average is : ", avg)
+
+
+
